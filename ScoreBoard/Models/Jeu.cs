e761c558eb67383f0a06df94b610b16731c518d7
Marc-Olivier Auger <116024612+Azazzoo@users.noreply.github.com>
2023-07-07 12:37:03 -0400

    Installation NuGetPackage Creation de la classe CatalogueDbContext

--- ScoreBoard/Models/Jeu.cs
+++ ScoreBoard/Models/Jeu.cs
@@ -1,14 +1,25 @@
-﻿namespace ScoreBoard.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScoreBoard.Models
 {
     public class Jeu
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Nom { get; set; }
+        [Required]
         public DateTime DateSortie { get; set; }
+        [Required]
         public string Description { get; set; }
+        [Required]
         public int JoueurId { get; set; }
+        [Required]
         public Joueur Joueur { get; set; }
+        [Required]
+        [Range(0, 100, ErrorMessage = "Le score doit etre comprit entre 0 et 100.")]
         public int ScoreJoueur { get; set; }
+        [Required]
         public DateTime DateEnregistrement { get; set; }
     }
 }
--- ScoreBoard/Models/Joueur.cs
+++ ScoreBoard/Models/Joueur.cs
@@ -1,13 +1,25 @@
-﻿namespace ScoreBoard.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScoreBoard.Models
 {
     public class Joueur
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [RegularExpression("^.{2,20}$", ErrorMessage = "Le nom doit comporter entre 2 et 20 caracteres")]
         public string Nom { get; set; }
+        [RegularExpression("^.{2,20}$", ErrorMessage = "Le prénom doit comporter entre 2 et 20 caracteres")]
+        [Required]
         public string Prenom { get; set; }
+        [RegularExpression("^[A-Z]{2,4}$", ErrorMessage = "L'équipe doit avoir 2 à 4 lettres majuscules.")]
         public string? Equipe { get; set; }
+
         public string? Telephone { get; set; }
+        [Required]
+        [Display(Name = "Identifiant")]
         public string Courriel { get; set; }
+        [Required]
         public List<Jeu>? Jeux { get; set; }
     }
 }
